13f4b29450a4e2e8ef1f92010ffa99ccf7604790
Keymandll <zsolt.imre@dcnws.com>
2016-07-12 22:09:54 -0400

    feat(primitive): added padding primitive. Fixed sizer.

--- engine2/primitives/sizer.py
+++ engine2/primitives/sizer.py
@@ -1,4 +1,5 @@
 import struct
+from utils import utils
 from primitives.__primitive import __primitive__
 
 all_properties = [
@@ -17,6 +18,13 @@ all_properties = [
         "error": "primitive requires fuzzable to be of type bool (1 or 0)"
     },
     {
+        "name": "full_range",
+        "type": "bool",
+        "values": [0, 1],
+        "default": 0,
+        "error": "primitive requires full_range to be of type bool (1 or 0)"
+    },
+    {
         "name": "inclusive",
         "type": "bool",
         "values": [0, 1],
@@ -30,6 +38,14 @@ all_properties = [
         "value": 0,
         "mandatory": 0,
         "error": "primitive requires offset to be of type type int or long"
+    },
+    {
+        "name": "value",
+        "type": ["int", "long"],
+        "default": 0,
+        "value": 0,
+        "mandatory": 0,
+        "error": "primitive requires value to be of type type int or long"
     },
     {
         "name": "endian",
@@ -81,46 +97,33 @@ class sizer(__primitive__):
     # -------------------------------------------------------------------------
 
     def init_library(self):
-        bitw = self.get('size') * 8
-        max = utils.bin_to_dec("1" + "0" * bitw)
-        if self.signed:
-            max = utils.bin_to_dec("1" + "0" * bitw) / 2 - 1
-
-        if self.max_num and self.max_num > max:
-            raise Exception("%s primitive maximum value is %d" % (self.type, max))
-        if self.max_num == None or self.max_num == 0:
-            self.max_num = max
+        max = utils.bin_to_dec("1" + "0" * (self.get('size') * 8))
 
         if self.full_range:
-            for i in xrange(0, self.max_num + 1):
+            for i in xrange(0, max + 1):
                 if i not in self.library: self.library.append(i)
         else:
             self.library += utils.integer_boundaries(
                 self.library,
-                self.max_num,
+                max,
                 0)
             self.library += utils.integer_boundaries(
                 self.library,
-                self.max_num,
-                self.max_num)
+                max,
+                max)
             for v in [2, 3, 4, 8, 16, 32]:
                 self.library += utils.integer_boundaries(
                     self.library,
-                    self.max_num,
-                    self.max_num / v)
+                    max,
+                    max / v)
 
     # -------------------------------------------------------------------------
     #
     # -------------------------------------------------------------------------
 
-    def render(self):
-        b = self.search(self.block)
-        length = len(b.render())
-        if self.get('inclusive'): length += self.get('size')
-        if self.get('offset'): length += self.get('offset')
-
+    def format(self, value):
         if self.get('format') == "ascii":
-            return str(length)
+            return str(value)
         else:
             endian = ">"
             if self.get('endian') == "little":
@@ -135,8 +138,24 @@ class sizer(__primitive__):
                 format = "Q"
 
             try:
-                return struct.pack(endian + format, length)
+                return struct.pack(endian + format, value)
             except Exception, ex:
                 raise Exception('failed to render sizer %s (%s)' %\
                 (self.name,  str(ex)))
+
+    # -------------------------------------------------------------------------
+    #
+    # -------------------------------------------------------------------------
+
+    def render(self):
+        if self.fuzzable and not self.complete:
+            value = super(sizer, self).render()
+            return self.format(value)
+
+        b = self.search(self.block)
+        length = len(b.render())
+        if self.get('inclusive'): length += self.get('size')
+        if self.get('offset'): length += self.get('offset')
+
+        return self.format(length)
 

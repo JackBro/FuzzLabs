667dc2ed002a5ac3b81e02553b6a9213aad615e9
Keymandll <zsolt.imre@dcnws.com>
2016-09-25 22:15:35 +0100

    feat(resources): added dynamic engine API resource loading based on endpoints.json

--- engine-new/engine/engine.py
+++ engine-new/engine/engine.py
@@ -3,62 +3,37 @@
 
 import os
 import inspect
+import importlib
 from OpenSSL import SSL
 from flask import Flask
 from flask_restful import Api
 from classes.Utils import Utils
 from classes.Config import Config
-from classes.ResourceInitAcl import ResourceInitAcl
-from classes.ResourceInitAPIKey import ResourceInitAPIKey
-from classes.ResourceSetupSsl import ResourceSetupSsl
-from classes.ResourcePing import ResourcePing
-from classes.ResourceShutdown import ResourceShutdown
-from classes.ResourceRemove import ResourceRemove
-from classes.ResourceAclList import ResourceAclList
-from classes.ResourceAclAdd import ResourceAclAdd
-from classes.ResourceAclRemove import ResourceAclRemove
-from classes.ResourceListJobs import ResourceListJobs
 
 ROOT = os.path.dirname(
             os.path.abspath(
                 inspect.getfile(inspect.currentframe()
             )))
 config = Config(ROOT, "/config/config.json")
-
+endpoints = None
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(ResourceInitAcl,
-                 '/setup/acl',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceSetupSsl,
-                 '/setup/ssl',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceInitAPIKey,
-                 '/setup/apikey',
-                 resource_class_kwargs=config)
-api.add_resource(ResourcePing,
-                 '/management/ping',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceShutdown,
-                 '/management/shutdown',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceRemove,
-                 '/management/remove',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceAclList,
-                 '/management/acl/list',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceAclAdd,
-                 '/management/acl/add',
-                 resource_class_kwargs=config)
-api.add_resource(ResourceAclRemove,
-                 '/management/acl/remove',
-                 resource_class_kwargs=config)
+try:
+    endpoints = Utils.read_json(ROOT + "/config/endpoints.json")
+except Exception, ex:
+    raise Exception('failed to load endpoints configuration: %s' %\
+                    str(ex))
 
-api.add_resource(ResourceListJobs,
-                 '/jobs',
-                 resource_class_kwargs=config)
+for endpoint in endpoints:
+    ep_name  = endpoint.keys()[0]
+    ep_cname = endpoint[ep_name]
+    ep_mod   = importlib.import_module("classes." +\
+                                       ep_cname)
+    ep_class = getattr(ep_mod, ep_cname)
+    api.add_resource(ep_class,
+                     ep_name,
+                     resource_class_kwargs=config)
 
 def start_engine():
     context = None
